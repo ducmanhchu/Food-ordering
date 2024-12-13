1b1cd0513935c7bdcdf01e850431a4f80b7c656a
Quynh Phan <qphan204@gmail.com>
2024-12-13 18:16:14 +0700

    update model Businessinfo, create api "get_business_info", "get_discount_codes", "get_payment_methods", fix api "create_order"

--- backend/Web/models.py
+++ backend/Web/models.py
@@ -377,7 +377,7 @@ class BusinessInfo(models.Model):
     name = models.CharField(max_length=255)
     address = models.TextField(null=True, blank=True)
     phone = models.CharField(max_length=10, null=True, blank=True)
-    
+    zalo_link = models.URLField(max_length=255, null=True, blank=True)
     def __str__(self):
         return self.name
     
